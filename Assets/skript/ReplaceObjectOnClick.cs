06111df0e06ba8ca6ff14cffecf62d359640945a
jelena viskovic <jelena.viskovic@gmail.com>
2020-11-13 14:39:15 +0000

    object replacement script

---
+++ Assets/skript/ReplaceObjectOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceObjectOnClick : MonoBehaviour
+{
+
+    public GameObject objectToReplace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectToReplace.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        objectToReplace.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
